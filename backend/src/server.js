ed15d081a62dbab2324eaba00aa36ac23a53e508
IT-9135 <tuminhhung0901@gmail.com>
2026-03-15 09:01:09 +0700

    fetch thong tin user

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/authRoute.js";
 import userRoute from "./routes/userRoute.js";
 import cookieParser from "cookie-parser";
 import { protectedRoute } from "./middlewares/authMiddleware.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ const PORT = process.env.PORT || 5001;
 // middlewares
 app.use(express.json()); // middleware này sẽ giúp express hiểu và đọc được req.body dưới dạng JSON
 app.use(cookieParser());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 // public routes - thuộc phần Authentication
 app.use("/api/auth", authRoute);
